0e50c9ce2339c90a54a90c64cda452a06631bf21
PYongju <wns82978@naver.com>
2026-03-02 15:27:49 +0900

    연결문자열 설정

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import models
 app = FastAPI()
 
 # --- [1. Azure Blob Storage 설정] ---
-AZURE_STORAGE_CONNECTION_STRING = "여기에_실제_연결_문자열_입력"
+AZURE_STORAGE_CONNECTION_STRING = "***REMOVED***"
 CONTAINER_NAME = "images" 
 
 try:
