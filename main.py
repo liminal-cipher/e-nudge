4797952fd4a2f2fecd6acead6aedb6f0ddbd7d45
PYongju <wns82978@naver.com>
2026-03-02 14:42:06 +0900

    이미지 달 수 있게 수정

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 # --- [Azure Blob Storage 설정] ---
 # ⭐️ 메모장에 적어둔 '연결 문자열'을 여기에 붙여넣으세요!
-AZURE_STORAGE_CONNECTION_STRING = "여기에_복사한_연결_문자열_입력"
+AZURE_STORAGE_CONNECTION_STRING = "***REMOVED***"
 CONTAINER_NAME = "images" 
 
 # 클라이언트 초기화
